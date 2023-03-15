d2227914a955e2c4e77653c2e3766c58572af5eb
clive-cudi <clivemaina41@gmail.com>
2023-03-15 14:07:05 +0300

    removed-unused-imports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,9 @@
 package com.company;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        CalcFrame frame = new CalcFrame("Interest Calculator", 420, 500);
+        new CalcFrame("Interest Calculator", 420, 500);
     }
 }
